15fd231a1813425c90decad14622d41e6fdb49f3
Sweetdeal <fettre@yahoo.de>
2023-03-23 00:55:07 +0100

    Init release

---
+++ README.md
@@ -0,0 +1,48 @@
+# DevTracker - Discord Bot
+
+### Features
+
+- Tracks Messages of a specific Role Name or ID
+- Post Messages of tracked roles in channel(s) you like!
+
+### Example
+
+![Example](https://i.imgur.com/f4KELUP_d.webp?maxwidth=760&fidelity=grand "Example Image of the Discord Bot")
+
+
+
+### Setup / Requirements
+https://pypi.org/project/discord.py/
+
+### Usage
+    1. Replace <Token here> with your Discord Bot Token without <> in bot.py
+    2. Modify rolesToWatch to your needs
+    3. Set useRoleIds to either True or False, depending on your rolesToWatch Setup.
+
+    For more Details, check examples.
+
+### Examples
+
+          
+        
+````python
+TOKEN = '12512515.5125125' # example
+useRoleIds = True
+rolesToWatch = [{'role': 1088148992514338836, 'channels': [ 1088231599025422356, 1088231613298647101]}]
+
+
+TOKEN = '12512515.5125125' # example
+useRoleIds = False
+rolesToWatch =[{'role': 'Developer', 'channels': [ 1088231599025422356, 1088231613298647101]},{'role': 'TestRole', 'channels': [ 1088231599025422356]}]
+````
+
+### Others
+If you want to modify the Message of the Bot, I recommend reading:
+https://python.plainenglish.io/python-discord-bots-formatting-text-efca0c5dc64a
+
+
+### Credits
+inspired by https://github.com/OniSensei/Role-Message-Tracker
+Who got inspired by me ^.^
+
+Wanted to give it a shot myself, to get an Open Source Version up and running in Python.
---
+++ bot.py
@@ -0,0 +1,87 @@
+import discord
+
+#
+# Powered by Feylinchen#1234
+# inspired by https://github.com/OniSensei/Role-Message-Tracker
+# who was inspired by Me ^.^
+# open Source Python Variant
+#
+# Multiple Roles + Channels are supported!
+#
+# Usage:
+# 1. TOKEN = Set your Bot Token
+# 2. Modify rolesToWatch by your needs.
+#   Either use IDs of Roles, to track invisible Roles or use Role Names.
+#   If you want to use IDs, make sure "useRoleIDs" is True
+#
+# Example with Names:
+# [{'role': 'Developer', 'channels': [ 1088231599025422356, 1088231613298647101]}]
+#
+#
+# Example with IDs:
+# rolesToWatch = [{'role': 1088148992514338836, 'channels': [ 1088231599025422356, 1088231613298647101]}]
+#
+#
+#
+# To Modify the Style of the Message
+# I recommend reading: https://python.plainenglish.io/python-discord-bots-formatting-text-efca0c5dc64a
+# Chapter: Multiline Code Blocks
+
+
+TOKEN = '<Your Token Here>'
+useRoleIds = True
+
+rolesToWatch = [{'role': 1088148992514338836, 'channels': [1088231599025422356, 1088231613298647101]},
+                {'role': 662383376115957770, 'channels': [1088231675605033040]}]
+
+
+def search(role, message):
+    if useRoleIds:
+        for p in message.author.roles:
+            if int(p.id) == role:
+                return True;
+        return False;
+    else:
+        for p in message.author.roles:
+            if str(p) == role.strip():
+                print("true")
+                return True
+        return False
+
+
+def run_discord_bot():
+    intents = discord.Intents.all()
+
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        for item in rolesToWatch:
+            if search(item.get('role'), message):
+                channels = item.get('channels')
+                for cn in channels:
+                    channel = client.get_channel(cn)
+                    embed = discord.Embed(
+                        description=message.content,
+                        color=discord.Color.blue())
+                    embed.set_author(name=str(message.author)[:-5],
+                                     icon_url="https://playpaxdei.com/_next/image?url=%2Fstatic%2Fimages%2Fpaxdei"
+                                              "-monogram-silver.png&w=64&q=75")
+                    embed.set_thumbnail(url=message.author.avatar)
+                    embed.add_field(name="Jump to Original Message:", value="[View](" + message.jump_url + ")", inline=False)
+                    await channel.send(embed=embed)
+        if message.content.startswith('hi'):
+            await message.channel.send('Hello!')
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel.id)
+        print(f"{username} said: '{user_message}' ({channel})")
+
+    client.run(TOKEN)
